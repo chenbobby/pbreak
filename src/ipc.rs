aca5af80bd8dadb94ca46bd83cedca51e3d73511
Bobby Chen <cb168.github@fastmail.com>
2024-12-01 05:59:05 +0000

    Adds tests for ipc::Pipe

--- src/ipc.rs
+++ src/ipc.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::ffi::CStr;
 
 pub struct Pipe {
     read_fd: libc::c_int,
@@ -9,10 +9,8 @@ impl Pipe {
     pub unsafe fn new() -> Pipe {
         let mut fds = vec![0; 2];
         if libc::pipe2(fds.as_mut_ptr(), 0) < 0 {
-            let errno_message = CString::from_raw(libc::strerror(*libc::__errno_location()))
-                .into_string()
-                .unwrap();
-            panic!("failed to open pipe: {}", errno_message);
+            let errno_message = CStr::from_ptr(libc::strerror(*libc::__errno_location()));
+            panic!("failed to open pipe: {:?}", errno_message);
         };
 
         return Pipe {
@@ -23,32 +21,31 @@ impl Pipe {
 
     // Reads a string out of the pipe.
     pub unsafe fn receive(&self) -> String {
-        let mut s = String::with_capacity(128);
-        if libc::read(
+        let mut buffer = vec![0; 128];
+
+        let n_bytes = libc::read(
             self.read_fd,
-            s.as_mut_ptr() as *mut libc::c_void,
-            s.capacity(),
-        ) < 0
-        {
-            let errno_message = CString::from_raw(libc::strerror(*libc::__errno_location()))
-                .into_string()
-                .unwrap();
+            buffer.as_mut_ptr() as *mut libc::c_void,
+            buffer.capacity(),
+        );
+
+        if n_bytes < 0 {
+            let errno_message = CStr::from_ptr(libc::strerror(*libc::__errno_location()));
             panic!(
-                "failed to read from pipe fd ({}): {}",
+                "failed to read from pipe fd ({}): {:?}",
                 self.read_fd, errno_message,
             );
         }
-        return s;
+
+        return String::from_utf8_lossy(&buffer[..n_bytes as usize]).to_string();
     }
 
     // Sends a string into a pipe.
     pub unsafe fn send(&self, s: &str) {
         if libc::write(self.write_fd, s.as_ptr() as *const libc::c_void, s.len()) < 0 {
-            let errno_message = CString::from_raw(libc::strerror(*libc::__errno_location()))
-                .into_string()
-                .unwrap();
+            let errno_message = CStr::from_ptr(libc::strerror(*libc::__errno_location()));
             panic!(
-                "failed to write into pipe fd ({}): {}",
+                "failed to write into pipe fd ({}): {:?}",
                 self.write_fd, errno_message
             );
         }
@@ -58,11 +55,9 @@ impl Pipe {
     pub unsafe fn close_receiver(&mut self) {
         if self.read_fd != -1 {
             if libc::close(self.read_fd) < 0 {
-                let errno_message = CString::from_raw(libc::strerror(*libc::__errno_location()))
-                    .into_string()
-                    .unwrap();
+                let errno_message = CStr::from_ptr(libc::strerror(*libc::__errno_location()));
                 panic!(
-                    "failed to close pipe's read file descriptor: {}",
+                    "failed to close pipe's read file descriptor: {:?}",
                     errno_message,
                 );
             };
@@ -75,11 +70,9 @@ impl Pipe {
     pub unsafe fn close_sender(&mut self) {
         if self.write_fd != -1 {
             if libc::close(self.write_fd) < 0 {
-                let errno_message = CString::from_raw(libc::strerror(*libc::__errno_location()))
-                    .into_string()
-                    .unwrap();
+                let errno_message = CStr::from_ptr(libc::strerror(*libc::__errno_location()));
                 panic!(
-                    "failed to close pipe's write file descriptor: {}",
+                    "failed to close pipe's write file descriptor: {:?}",
                     errno_message,
                 );
             };
@@ -94,6 +87,37 @@ impl Drop for Pipe {
         unsafe {
             self.close_receiver();
             self.close_sender();
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::ipc::Pipe;
+
+    #[test]
+    fn new_and_drop_succeeds() {
+        unsafe {
+            Pipe::new();
+        }
+    }
+
+    #[test]
+    fn send_and_receive_succeeds() {
+        unsafe {
+            let pipe = Pipe::new();
+            let s = "message";
+            pipe.send(s);
+            assert_eq!(pipe.receive(), s);
+        }
+    }
+
+    #[test]
+    fn close_succeeds() {
+        unsafe {
+            let mut pipe = Pipe::new();
+            pipe.close_receiver();
+            pipe.close_sender();
         }
     }
 }
